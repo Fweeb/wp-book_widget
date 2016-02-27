f276ae7e76a18636537de2549cb549efb7ae6c41
Jason van Gumster <jason@monsterjavaguns.com>
2016-02-27 17:16:06 -0500

    Was using and old/bad plugin template. This should be better now.

--- book_widget.php
+++ book_widget.php
@@ -9,27 +9,48 @@ Author URI: http://monsterjavaguns.com
 License: GPL3
 */
 
-class book_widget extends WP_Widget {
+class Book_Widget extends WP_Widget {
 
-    function book_widget() {
+    public function __construct() {
+        $widget_ops = array(
+            'classname' => 'book_widget',
+            'description' => 'A simple widget for adding links to your books for sale on sites like Amazon, Kobo, Nook, etc.',
+        );
+        parent::__construct( 'book_widget', 'Book Widget', $widget_ops );
+    }
+
+    /**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+    public function widget( $args, $instance ) {
 
     }
 
-    function form($instance) {
+    /**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+    public function form($instance) {
 
     }
 
+    /**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
     function update($new_instance, $old_instance) {
-
-    }
-
-    function widget{$args, $instance) {
 
     }
 
 }
 
-add_action('widgets_init', create_function('', 'return
-    register_widget("book_widget");'));
+add_action('widgets_init',
+    create_function('', 'return register_widget("Book_Widget");'));
 
 ?>
