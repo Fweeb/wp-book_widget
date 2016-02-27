67d26c0b2706a24a3026a9e28659c5d7637c0353
Jason van Gumster <jason@monsterjavaguns.com>
2016-02-27 17:39:42 -0500

    Filled in the rest of the simple boilerplate stuff. Now to actually make the widget do something interesting.

--- book_widget.php
+++ book_widget.php
@@ -9,6 +9,10 @@ Author URI: http://monsterjavaguns.com
 License: GPL3
 */
 
+// Block direct requests
+if( !defined('ABSPATH') )
+    die('-1');
+
 class Book_Widget extends WP_Widget {
 
     public function __construct() {
@@ -26,7 +30,12 @@ class Book_Widget extends WP_Widget {
 	 * @param array $instance
 	 */
     public function widget( $args, $instance ) {
-
+        echo $args['before_widget'];
+        if ( ! empty( $instance['title'] ) ) {
+            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+        }
+        echo __( 'Test', 'text_domain' ); //XXX
+        echo $args['after_widget'];
     }
 
     /**
@@ -34,8 +43,14 @@ class Book_Widget extends WP_Widget {
 	 *
 	 * @param array $instance The widget options
 	 */
-    public function form($instance) {
-
+    public function form( $instance ) {
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+        ?>
+        <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+        </p>
+        <?php
     }
 
     /**
@@ -44,11 +59,14 @@ class Book_Widget extends WP_Widget {
 	 * @param array $new_instance The new options
 	 * @param array $old_instance The previous options
 	 */
-    function update($new_instance, $old_instance) {
+    function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 
+        return $instance;
     }
 
-}
+} // class Book_Widget
 
 add_action('widgets_init',
     create_function('', 'return register_widget("Book_Widget");'));
